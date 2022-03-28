1592ba0ee41ccda33a7be00f73b1436425bf84c0
dolomita28 <carlos.mas28@gmail.com>
2022-03-28 19:52:48 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const btnRound = document.querySelectorAll('[data-btn]');
+const btnSubmit = document.querySelector('btn-submit');
+
+
+
+btnRound.forEach(btn=>{
+    btn.addEventListener('click',(event)=>{
+        event.preventDefault();
+        
+    })
+})
+
+btnSubmit.addEventListener('click',()=>{
+
+})
