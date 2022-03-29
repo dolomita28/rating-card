639ba07d502b10beebe30245fa22119a36b1e264
dolomita28 <carlos.mas28@gmail.com>
2022-03-29 12:57:06 +0200

    challenge completed

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
 const btnRound = document.querySelectorAll('[data-btn]');
-const btnSubmit = document.querySelector('btn-submit');
+const btnSubmit = document.querySelector('.btn-submit');
+const cards = document.querySelectorAll('.card');
+const score = document.querySelector('.score');
 
-
+const updateScore = (scoreTxt) => {
+    score.insertAdjacentHTML('beforeend', scoreTxt)
+}
 
 btnRound.forEach(btn=>{
     btn.addEventListener('click',(event)=>{
-        event.preventDefault();
-        
+        event.preventDefault();        
+        updateScore(`You selected ${event.target.innerText} out of 5`);
     })
 })
 
-btnSubmit.addEventListener('click',()=>{
-
+btnSubmit.addEventListener('click',()=>{   
+    cards.forEach(card => {        
+        card.classList.toggle('hidden');       
+    })
 })
+
